2d7e5dd24985726a3aec7199b2ee3ba652ddc56d
Florian Jüttner <florian.juettner@telefonica.com>
2026-08-20 11:59:23 +0200

    allow to print version as json

--- cmd/gobalprotect/version.go
+++ cmd/gobalprotect/version.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	cli "github.com/urfave/cli/v3"
 )
@@ -11,7 +13,34 @@ func versionCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "version",
 		Usage: "Print version information",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "json",
+				Aliases: []string{"j"},
+				Usage:   "Output version information as JSON",
+			},
+			&cli.BoolFlag{
+				Name:    "pretty",
+				Aliases: []string{"p"},
+				Usage:   "Output version information as pretty-printed",
+			},
+		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
+			if cmd.Bool("json") {
+				info := map[string]string{
+					"name":      "gobalprotect",
+					"version":   version,
+					"buildDate": buildDate,
+				}
+				enc := json.NewEncoder(os.Stdout)
+				if cmd.Bool("pretty") {
+					enc.SetIndent("", "  ")
+				}
+				if err := enc.Encode(info); err != nil {
+					return err
+				}
+				return nil
+			}
 			fmt.Printf("gobalprotect %s (built %s)\n", version, buildDate)
 			return nil
 		},
